9d59339851d751fb28d4cc9ef2f4ed1497ae8475
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2026-01-27 10:18:25 -0500

    Setup GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /*
+    Code principal gérant les transitions entre les scènes.
+    Le GameManager est toujours présent dans la hiérarchie, dans toutes les scènes.
+    */
+
+
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            // S'assurer que cette instance n'est pas supprimée entre les scènes
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+}
