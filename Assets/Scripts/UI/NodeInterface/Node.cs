9a9e3f512d54755f83de3cdf44f6672f823591c5
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2026-02-22 17:05:42 -0500

    started interface

---
+++ Assets/Scripts/UI/NodeInterface/Node.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    
+}
+
+public enum InputOutputType
+{
+    Int,
+    Float,
+    SliderFloat,
+    Texture,
+    Color,
+    Curve
+}
