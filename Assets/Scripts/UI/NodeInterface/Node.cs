ac47af2e10b6baae3554e8862097307657a14735
ManechLepage <113209819+ManechLepage@users.noreply.github.com>
2026-02-24 10:40:03 -0500

    Big UI update

--- Assets/Scripts/UI/NodeInterface/Node.cs
+++ Assets/Scripts/UI/NodeInterface/Node.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
 
-public class Node : MonoBehaviour
+[CreateAssetMenu(fileName = "New Node", menuName = "Node")]
+public class Node : ScriptableObject
 {
-    
+    public string nodeName;
+    public List<InputOutputType> inputs = new List<InputOutputType>();
+    public List<InputOutputType> outputs = new List<InputOutputType>();
+}
+
+public enum NodeType
+{
+    Algorithm,
+    Function,
+    Output
 }
 
 public enum InputOutputType
@@ -10,7 +23,10 @@ public enum InputOutputType
     Int,
     Float,
     SliderFloat,
+    Vector2,
+    Heightmap,
     Texture,
     Color,
+    Gradient,
     Curve
 }
