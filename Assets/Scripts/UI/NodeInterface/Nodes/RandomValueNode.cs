befa3bc827c6c454721546336616409ef0fd227a
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2026-03-14 20:15:33 -0400

    Working random value!

---
+++ Assets/Scripts/UI/NodeInterface/Nodes/RandomValueNode.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class RandomValueNode : NodeBehaviour
+{
+    public override Variant Fire()
+    {
+        Vector2 range = GetInputValue("range").GetValue<Vector2>();
+        float randomValue = Random.Range(range.x, range.y);
+        return new Variant(randomValue);
+    }
+}
